93f8c0569060d47f91a2ee95c80812f728a7ddb4
Sofia Weng <sowe0001@stud.kea.dk>
2024-02-17 13:54:47 +0100

    opstart på sider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//----------------burgermenu---------------
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navmenu.classList.toggle("active");
+});
+//sikrer at menuknap forsvinder
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navmenu.classList.remove("active");
+  })
+);
