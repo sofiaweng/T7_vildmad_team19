09b87c557c30dc68a54678bff120111f53367ca4
vabr0002 <vabr0002@stud.kea.dk>
2024-02-21 14:02:58 +0100

    popup_info & footer

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,31 @@
 function openPopup(imgElement) {
   const modal = document.getElementById("myModal");
-  const popupHeader = document.getElementById("popupHeader");
-  const popupText = document.getElementById("popupText");
-  const popupSubheader = document.getElementById("popupSubheader");
+  const popupTitle = document.getElementById("title");
+  const popupDescription = document.getElementById("description");
+  const popupLandscape = document.getElementById("landscape");
+  const popupImage = document.getElementById("billede");
 
   modal.style.display = "block";
-  popupHeader.textContent = "Popup Header";
-  popupText.textContent = "This is a paragraph in the popup.";
-  popupSubheader.textContent = "Popup Subheader";
+  popupTitle.textContent = imgElement.querySelector(".title").textContent;
+
+  // Set the content of description and landscape elements
+  popupDescription.textContent =
+    imgElement.querySelector(".description").textContent;
+  popupLandscape.textContent =
+    imgElement.querySelector(".landscape").textContent;
+
+  popupImage.src = imgElement.querySelector(".gridItem").src;
 }
 
 function closePopup() {
   const modal = document.getElementById("myModal");
   modal.style.display = "none";
 }
+
+// Event listener to close the popup when clicking outside the modal
+document.getElementById("myModal").addEventListener("click", function (event) {
+  // Check if the clicked element is the modal overlay
+  if (event.target === this) {
+    closePopup(); // Close the popup if the clicked element is the overlay
+  }
+});
